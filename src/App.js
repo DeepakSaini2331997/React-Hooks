5688309d9595d6912d7812162109ec25f73d78e7
deepak <deepaksaini2331997@gmail.com>
2025-01-31 21:49:00 +0530

    use state hooks use in function components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Statecomp from "./components/usestate/statecomp";
+function App() {
+  return (
+    <div className="App">
+      <h1>
+       React Hooks
+      </h1><br></br>
+      <Statecomp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/usestate/statecomp.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react"
+
+const Statecomp = ()=>{
+    const [text,setText] = useState("This is default text");
+    const [button,setButton] = useState('')
+    return(
+        <div>
+           <h2>Discuss usestate</h2><hr></hr>
+           <p>
+            {
+                text
+            }
+            <br></br>
+            {
+                button?
+                <button onClick={()=>{setText('This is default text'); setButton(false)}}>change text</button>:
+                <button onClick={()=> {setText('Change Text is done'); setButton(true)}}>change text</button>
+            }
+            
+           </p>
+        </div>
+    )
+}
+
+export default Statecomp;
